8a1a304ea2d89e5f9636d5e466e558c98358dde6
dkwdaft <31158132+dkwdaft@users.noreply.github.com>
2023-08-12 12:04:51 +0100

    Complete Don't fall through

--- Don't fall through/Assets/Scripts/TileController.cs
+++ Don't fall through/Assets/Scripts/TileController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,11 +6,29 @@ using UnityEngine;
 public class TileController : MonoBehaviour
 {
     [SerializeField] Material startColour;
+    [SerializeField] Material safeColour;
+    [SerializeField] Material unsafeColour;
     Renderer rend;
     // Start is called before the first frame update
     void Start()
     {
         rend = GetComponent<Renderer>();
         rend.material = startColour;
+        GetComponent<Rigidbody>().useGravity = false;
+        GetComponent<Rigidbody>().isKinematic = true;
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (gameObject.tag == "Safe")
+        {
+            rend.sharedMaterial = safeColour;
+        }
+        else if (gameObject.tag == "Tile")
+        {
+            rend.sharedMaterial = unsafeColour;
+            GetComponent<Rigidbody>().useGravity = true;
+            GetComponent<Rigidbody>().isKinematic = false;
+        }
     }
 }
